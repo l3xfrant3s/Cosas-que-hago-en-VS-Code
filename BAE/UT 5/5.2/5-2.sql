c3d65d72762401ed1589b8fffbaae02ee59e3857
l3xfrant3s <l3xfrant3s@outlook.com>
2024-03-18 10:40:26 +0000

    18 de Marzo, cambio de carpeta en portatil

---
+++ BAE/UT 5/5.2/5-2.sql
@@ -0,0 +1,40 @@
+/*1*/
+create database agenda2019;
+/*2*/
+create user gerente identified by '123456ZZ$';
+grant all on agenda2019.* to gerente;
+
+
+/*4*/
+show grants;
+set password for gerente = '1234';
+/*5*/
+use agenda2019;
+create table Contactos(
+Cod int primary key,
+Nombre varchar(20),
+Apellido varchar(40),
+email varchar(40),
+FechaNac date
+);
+create table Telefonos(
+Cod int,
+Telefono varchar(11),
+constraint telefonos_pk primary key (Cod, Telefono),
+constraint telefonos_fk foreign key (Cod) references Contactos(Cod)
+                                                on update cascade
+);
+/*6*/
+create user agente;
+create user colega;
+grant grant option on *.* to gerente;
+/*7*/
+grant select(nombre, apellido) on agenda2019.contactos to colega;
+grant insert, update, delete on agenda2019.* to agente;
+/*8*/
+
+/*9*/
+revoke delete on agenda2019.* from agente;
+/*10*/
+
+/*11*/
--- BAE/UT 5/EXAMEN DML/Examen BAE Consultas.sql
+++ BAE/UT 5/EXAMEN DML/Examen BAE Consultas.sql
@@ -30,7 +30,7 @@ order by FechaNac desc;
 
 
 
-2)
+/*2)*/
 select Municipio, count(Repetidor) 'Número de Repetidores'
 from alumnos
 where Repetidor = 'SI'
@@ -41,14 +41,14 @@ order by count(Repetidor) desc, Municipio desc;
 
 
 
-3) 
+/*3)*/ 
 select substr(trim(DNI), 1, 8) NIF, substr(trim(DNI), 9, 1) 'Letra NIF',
 concat(trim(Nombre), '-', trim(Apellido)) 'Nombre Completo', FechaNac 'Fecha de Nacimiento'
 from alumnos
 where substr(trim(Nombre), -1, 1) = 'S' and substr(trim(Apellido), -1, 1) = 'Z';
 
 
-4) 
+/*4)*/
 select DNI, concat(trim(Nombre), ' ', trim(Apellido)) 'Nombre Completo', count(Nota)
 from notas N join alumnos A on N.CodAlum = A.CodAlum
 where Nota < 5
@@ -58,12 +58,12 @@ order by count(Nota) desc, DNI desc;
 
 
 
-5) 
+/*5)*/
 select NombreDep, D.email email, concat(trim(nombre), ' ', substr(Apellido, 1, 1), '.') 'Jefe/a Departamento'
 from departamento D left join profesor P on D.JefeDep = P.CodProfe;
 
 
-6)
+/*6)*/
 
 
 
@@ -76,27 +76,27 @@ from alumnos A, modulos M, notas N
 where A.CodAlum = N.CodAlum and M.CodMod = N.CodMod
 and Siglas = 'PRO' and Nota < 5;
 
-7) 
+/*7)*/
 select Siglas, Titulo, round(avg(nota), 2) 'Nota Media'
 from modulos m join notas n on m.CodMod = n.CodMod
 group by m.CodMod
 order by round(avg(nota), 2) desc;
 
-8)
+/*8)*/
 select a.CodAlum, Nombre, Apellido, Municipio, round(avg(nota), 2) 'Nota Media'
 from alumnos a join notas n on a.CodAlum = n.CodAlum
 group by a.CodAlum
 having round(avg(nota), 2) > (select avg(nota) from notas)
 order by round(avg(nota), 2) desc;
 
-9)
+/*9)*/
 select P.email 'Correos de profes de Informática y departamentos'
 from profesor P join departamento D on P.Dep = D.CodDep
 where substr(trim(NombreDep), 1, 6) = 'Inform' and substr(trim(NombreDep), 8, 4) = 'tica'
 union
 select email from departamento;
 
-10)
+/*10)*/
 select Nombre, Apellido, Sueldo, year(curdate()) - year(FechaIngreso) Antigüedad
 from profesor
 where year(curdate()) - year(FechaIngreso) > 15;
