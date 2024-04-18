795678083988cf867f0841bf3fae235b340669ac
l3xfrant3s <l3xfrant3s@outlook.com>
2024-04-18 13:20:04 +0100

    18 de abril, examenes

---
+++ PRO/prueba.java
@@ -0,0 +1,21 @@
+package PRO;
+
+public class prueba{
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4};
+        int[] b = new int[4];
+        /*for(int i = 0; i < a.length; i++){
+            b[i] = a[i];
+        }*/
+        b = a;
+        for(int i = 0; i < a.length; i++){
+            System.out.println(a[i] + " ");
+        }
+        b[0] = 5;
+        for(int i = 0; i < a.length; i++){
+            System.out.println(a[i] + ",");
+        }
+    }
+
+}
