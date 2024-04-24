70f165a30c94defb02fe084d18c020b04ebb8e22
l3xfrant3s <l3xfrant3s@outlook.com>
2024-04-24 12:48:51 +0100

    24 de Abril, antes de empezar SSF 6.2

--- PRO/!Prueba/prueba2.java
+++ PRO/!Prueba/prueba2.java
@@ -1,14 +1,14 @@
-package PRO;
 
-public class prueba{
+
+public class prueba2{
 
     public static void main(String[] args) {
         int[] a = {1,2,3,4};
         int[] b = new int[4];
-        /*for(int i = 0; i < a.length; i++){
+        for(int i = 0; i < a.length; i++){
             b[i] = a[i];
-        }*/
-        b = a;
+        }
+        //b = a;
         for(int i = 0; i < a.length; i++){
             System.out.println(a[i] + " ");
         }
@@ -17,5 +17,4 @@ public class prueba{
             System.out.println(a[i] + ",");
         }
     }
-
 }
